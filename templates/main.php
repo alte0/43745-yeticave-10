6232e51189706f5f655c0c4fa516e6ca25f62f63
alte0 <ctpagateji@yandex.ru>
2019-08-15 15:44:41 +0500

    удалил скобку

--- templates/main.php
+++ templates/main.php
@@ -22,7 +22,7 @@
         <img src="<?= $announcement["url-img"] ?>" width="350" height="260" alt="">
       </div>
       <div class="lot__info">
-        <span class="lot__category"><?= $announcement["category"]) ?></span>
+        <span class="lot__category"><?= $announcement["category"] ?></span>
         <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= clearStrDataTags($announcement["title"]) ?></a></h3>
         <div class="lot__state">
           <div class="lot__rate">
