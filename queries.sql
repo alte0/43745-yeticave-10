f11033feb868c65ba9c993a546ae15191f21dfb9
alte0 <ctpagateji@yandex.ru>
2019-08-21 10:46:31 +0500

    Поправил пути картинок

--- queries.sql
+++ queries.sql
@@ -16,12 +16,12 @@ INSERT INTO users (name, email, password, image, contacts)
 -- Добавляем список объявлений.
 INSERT INTO lots (name, description, start_price, image, step, date_completion, user_id, category_id)
   VALUES 
-    ('2014 Rossignol District Snowboard', 'Отличное состояние, как новый!', 10999, '/imgs/lot-1.jpg', 100, '2019-08-25 12:25', 1, 1),
-    ('DC Ply Mens 2016/2017 Snowboard', 'Немного краска слезла', 159999, '/imgs/lot-2.jpg', 1000, '2019-08-26 10:15', 1, 1),
-    ('Крепления Union Contact Pro 2015 года размер L/XL', 'Не разу не пользовался!', 8000, '/imgs/lot-3.jpg', 100, '2019-08-26 20:00', 1, 2),
-    ('Ботинки для сноуборда DC Mutiny Charocal', 'Эксклюзивные!', 10999, '/imgs/lot-4.jpg', 100, '2019-08-26 18:00', 1, 3),
-    ('Куртка для сноуборда DC Mutiny Charocal', 'Пару сезонов откатал!', 7500, '/imgs/lot-5.jpg', 100, '2019-08-26 15:00', 1, 4),
-    ('Маска Oakley Canopy', 'Отдаю почти даром!', 5400, '/imgs/lot-6.jpg', 50, '2019-08-26 13:00', 1, 6);
+    ('2014 Rossignol District Snowboard', 'Отличное состояние, как новый!', 10999, 'img/lot-1.jpg', 100, '2019-08-25 12:25', 1, 1),
+    ('DC Ply Mens 2016/2017 Snowboard', 'Немного краска слезла', 159999, 'img/lot-2.jpg', 1000, '2019-08-26 10:15', 1, 1),
+    ('Крепления Union Contact Pro 2015 года размер L/XL', 'Не разу не пользовался!', 8000, 'img/lot-3.jpg', 100, '2019-08-26 20:00', 1, 2),
+    ('Ботинки для сноуборда DC Mutiny Charocal', 'Эксклюзивные!', 10999, 'img/lot-4.jpg', 100, '2019-08-26 18:00', 1, 3),
+    ('Куртка для сноуборда DC Mutiny Charocal', 'Пару сезонов откатал!', 7500, '/img/lot-5.jpg', 100, '2019-08-26 15:00', 1, 4),
+    ('Маска Oakley Canopy', 'Отдаю почти даром!', 5400, 'img/lot-6.jpg', 50, '2019-08-26 13:00', 1, 6);
 -- Добавляем пару ставок для любого объявления.
 INSERT INTO bets (price, user_id, lot_id)
   VALUES 
