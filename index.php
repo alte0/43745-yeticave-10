3e05c662c40ad467f797df1d4d15fce8041fa6b9
alte0 <ctpagateji@yandex.ru>
2019-08-25 14:57:20 +0500

    Пправка 2

--- index.php
+++ index.php
@@ -2,28 +2,35 @@
 require "init.php";
 
 $content = '';
-$today = date_create('now');
+$today = date("Y-m-d H:i:s");
 
-$sqlAnnouncements = 'SELECT lots.*, c.name AS category FROM lots INNER JOIN сategories c ON lots.category_id = c.id LEFT JOIN bets b ON lots.id = b.lot_id WHERE lots.date_completion >= "$today" GROUP BY id ORDER BY date_create DESC';
+$sqlAnnouncements = "SELECT lots.*, c.name AS category FROM lots INNER JOIN сategories c ON lots.category_id = c.id LEFT JOIN bets b ON lots.id = b.lot_id WHERE lots.date_completion >= '$today' GROUP BY id ORDER BY date_create DESC";
 
 $resultAnnouncements = mysqli_query($linkDB, $sqlAnnouncements);
 
-if ($resultCategories && $resultAnnouncements) {
+if (!$resultCategories && !$resultAnnouncements) {
     $error = mysqli_error($linkDB);
-    $categories = mysqli_fetch_all($resultCategories, MYSQLI_ASSOC);
-    $announcements = mysqli_fetch_all($resultAnnouncements, MYSQLI_ASSOC);
-
-    $content = include_template(
-        'main.php',
-        [
-            "categories" => $categories,
-            "announcements" => $announcements,
-        ]
-    );
-} else {
-    $error = mysqli_error($linkDB);
-    $content = includeErrorTemplate($error);
+    showErrorTemplate([
+        "title" => "Ошибка - YetiCave",
+        "error" => $error,
+        "categories" => $categories,
+        "content" => $content,
+        "user_name" => $user_name,
+        "is_auth" => $is_auth
+    ]);
+    die;
 }
+
+$categories = mysqli_fetch_all($resultCategories, MYSQLI_ASSOC);
+$announcements = mysqli_fetch_all($resultAnnouncements, MYSQLI_ASSOC);
+
+$content = include_template(
+    'main.php',
+    [
+        "categories" => $categories,
+        "announcements" => $announcements,
+    ]
+);
 
 $layout = include_template(
     'layout.php',
